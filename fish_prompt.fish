ea67ac419bc8ab213bd7107c569264fe3b90152a
Will Stone <654103+will-stone@users.noreply.github.com>
2018-07-11 09:35:29 +0100

    Move all `set` to top of file

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -4,6 +4,21 @@ set fish_color_command green
 # Show un-truncated directory names in prompt
 set fish_prompt_pwd_dir_length 0
 
+# Git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch brblack
+set __fish_git_prompt_color_dirtystate yellow
+set __fish_git_prompt_color_stagedstate green
+set __fish_git_prompt_color_upstream cyan
+
+# Git Characters
+set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_stagedstate '⇢'
+set __fish_git_prompt_char_upstream_prefix ' '
+set __fish_git_prompt_char_upstream_ahead '⇡'
+set __fish_git_prompt_char_upstream_behind '⇣'
+
 function _print_in_color
   set -l string $argv[1]
   set -l color  $argv[2]
@@ -20,21 +35,6 @@ function _prompt_color_for_status
     echo red
   end
 end
-
-# Git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch brblack
-set __fish_git_prompt_color_dirtystate yellow
-set __fish_git_prompt_color_stagedstate green
-set __fish_git_prompt_color_upstream cyan
-
-# Git Characters
-set __fish_git_prompt_char_dirtystate '*'
-set __fish_git_prompt_char_stagedstate '⇢'
-set __fish_git_prompt_char_upstream_prefix ' '
-set __fish_git_prompt_char_upstream_ahead '⇡'
-set __fish_git_prompt_char_upstream_behind '⇣'
 
 function fish_prompt
   set -l last_status $status
