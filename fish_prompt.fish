fb18abb99ce6ad2b4ce7d988c5e08d5a804b84e1
Will Stone <654103+will-stone@users.noreply.github.com>
2018-07-11 09:28:18 +0100

    Use fish_git_prompt
    fish has an inbuilt git prompt and can be configured to look like Pure. This simplifies the file by the a long way.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,63 +1,8 @@
+# Colour available commands green instead of white
 set fish_color_command green
+
+# Show un-truncated directory names in prompt
 set fish_prompt_pwd_dir_length 0
-
-function _in_git_directory
-  git rev-parse --git-dir > /dev/null 2>&1
-end
-
-function _git_branch_name_or_revision
-  set -l branch (git symbolic-ref HEAD ^ /dev/null | sed -e 's|^refs/heads/||')
-  set -l revision (git rev-parse HEAD ^ /dev/null | cut -b 1-7)
-
-  if test (count $branch) -gt 0
-    echo $branch
-  else
-    echo $revision
-  end
-end
-
-function _git_upstream_configured
-  git rev-parse --abbrev-ref @"{u}" > /dev/null 2>&1
-end
-
-function _git_behind_upstream
-  test (git rev-list --right-only --count HEAD...@"{u}" ^ /dev/null) -gt 0
-end
-
-function _git_ahead_of_upstream
-  test (git rev-list --left-only --count HEAD...@"{u}" ^ /dev/null) -gt 0
-end
-
-function _git_dirty
-  set -l is_git_dirty (command git status --porcelain --ignore-submodules ^/dev/null)
-  test -n "$is_git_dirty"
-end
-
-function _git_upstream_status
-  set -l arrows
-
-  if _git_upstream_configured
-    if _git_behind_upstream
-      set arrows "$arrows⇣"
-    end
-
-    if _git_ahead_of_upstream
-      set arrows "$arrows⇡"
-    end
-  end
-
-  echo $arrows
-end
-
-function _git_status
-  set -l asterisk
-
-  if _git_dirty
-    set asterisk "$asterisk*"
-  end
-
-  echo $asterisk
-end
 
 function _print_in_color
   set -l string $argv[1]
@@ -76,16 +21,27 @@ function _prompt_color_for_status
   end
 end
 
+# Git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch brblack
+set __fish_git_prompt_color_dirtystate yellow
+set __fish_git_prompt_color_stagedstate green
+set __fish_git_prompt_color_upstream cyan
+
+# Git Characters
+set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_stagedstate '⇢'
+set __fish_git_prompt_char_upstream_prefix ' '
+set __fish_git_prompt_char_upstream_ahead '⇡'
+set __fish_git_prompt_char_upstream_behind '⇣'
+
 function fish_prompt
   set -l last_status $status
 
   _print_in_color "\n"(prompt_pwd) blue
 
-  if _in_git_directory
-    _print_in_color " "(_git_branch_name_or_revision) brblack
-    _print_in_color " "(_git_status) yellow
-    _print_in_color " "(_git_upstream_status) cyan
-  end
+  __fish_git_prompt " %s"
 
   _print_in_color "\n❯ " (_prompt_color_for_status $last_status)
 end
