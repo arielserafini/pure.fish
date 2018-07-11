8cced3b315d1e3c1a1e99da716ab513084bf5119
Will Stone <654103+will-stone@users.noreply.github.com>
2018-07-11 11:34:11 +0100

    remove git equal symbol

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -16,7 +16,7 @@ set __fish_git_prompt_color_upstream cyan
 set __fish_git_prompt_char_dirtystate '*'
 set __fish_git_prompt_char_stagedstate '⇢'
 set __fish_git_prompt_char_upstream_prefix ' '
-set __fish_git_prompt_char_upstream_equal '-'
+set __fish_git_prompt_char_upstream_equal ''
 set __fish_git_prompt_char_upstream_ahead '⇡'
 set __fish_git_prompt_char_upstream_behind '⇣'
 set __fish_git_prompt_char_upstream_diverged '⇡⇣'
